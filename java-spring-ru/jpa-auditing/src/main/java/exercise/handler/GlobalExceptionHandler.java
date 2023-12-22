062b19d62cfb75fa3baae8632a27a2555bf6f22d
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 18:08:10 +0300

    Add error handler

--- java-spring-ru/jpa-auditing/src/main/java/exercise/handler/GlobalExceptionHandler.java
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package exercise.handler;
 
+import exercise.exception.ResourceAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,5 +15,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
