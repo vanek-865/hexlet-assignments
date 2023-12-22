c6b8b78e608d7776609b359a62e42f82b02185dd
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 17:14:20 +0300

    Add product post controller

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
@@ -1,23 +1,22 @@
 package exercise.controller;
 
+import exercise.exception.ResourceNotFoundException;
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import exercise.model.Product;
-import exercise.repository.ProductRepository;
-import exercise.exception.ResourceNotFoundException;
-import exercise.exception.ResourceAlreadyExistsException;
 
 @RestController
 @RequestMapping("/products")
@@ -32,21 +31,35 @@ public class ProductsController {
     }
 
     // BEGIN
-    
+    @PostMapping(path = "")
+    public ResponseEntity<String> update(@RequestBody Product productData) {
+        List<Product> products = productRepository.findAll().stream().filter(p -> (p.equals(productData))).toList();
+
+        if (products.isEmpty()) {
+            productRepository.save(productData);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(productData.toString());
+        }else
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("Product with already exists, id=" + products.get(0).getId());
+
+    }
     // END
 
     @GetMapping(path = "/{id}")
     public Product show(@PathVariable long id) {
-        var product =  productRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
 
         return product;
     }
 
     @PutMapping(path = "/{id}")
     public Product update(@PathVariable long id, @RequestBody Product productData) {
-        var product =  productRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
 
         product.setTitle(productData.getTitle());
         product.setPrice(productData.getPrice());
