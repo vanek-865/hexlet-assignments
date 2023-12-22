e74f9ebf0425b5cdc667ab433106e9a155fa4b1b
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 18:08:34 +0300

    Add task controller api

--- java-spring-ru/jpa-auditing/src/main/java/exercise/controller/TasksController.java
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/controller/TasksController.java
@@ -1,22 +1,22 @@
 package exercise.controller;
 
+import exercise.exception.ResourceAlreadyExistsException;
+import exercise.exception.ResourceNotFoundException;
+import exercise.model.Task;
+import exercise.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import exercise.model.Task;
-import exercise.repository.TaskRepository;
-import exercise.exception.ResourceNotFoundException;
 
 @RestController
 @RequestMapping("/tasks")
@@ -33,14 +33,35 @@ public class TasksController {
     @GetMapping(path = "/{id}")
     public Task show(@PathVariable long id) {
 
-        var task =  taskRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
 
         return task;
     }
 
     // BEGIN
-    
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task create(@RequestBody Task task) {
+        if (taskRepository.findAll().contains(task)) {
+            throw new ResourceAlreadyExistsException("Task " + task.getTitle() + " already exists");
+        }
+        return taskRepository.save(task);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Task update(@PathVariable long id, @RequestBody Task taskData) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+
+        task.setTitle(taskData.getTitle());
+        task.setDescription(taskData.getDescription());
+
+        taskRepository.save(taskData);
+
+        return taskRepository.save(taskData);
+    }
+
     // END
 
     @DeleteMapping(path = "/{id}")
