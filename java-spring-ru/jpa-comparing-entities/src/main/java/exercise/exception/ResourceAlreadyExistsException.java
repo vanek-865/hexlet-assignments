892cba6fab1421809d26314f05f831b56ee7511d
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 17:12:56 +0300

    Add resource already exists exception

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package exercise.exception;
 
 // BEGIN
-
+public class ResourceAlreadyExistsException extends RuntimeException{
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
 // END
