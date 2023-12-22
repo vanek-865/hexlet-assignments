1e1ef0d631069f89b9f1fd5a1bdffe678f88e813
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 15:50:27 +0300

    Init

---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package exercise.exception;
+
+// BEGIN
+
+// END
---
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -0,0 +1,15 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+
+// END
