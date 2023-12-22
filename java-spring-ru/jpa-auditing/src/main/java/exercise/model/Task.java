7c0c4bfe8191a04e3684bc5a32af569886c7b28f
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-22 17:28:47 +0300

    Init

---
+++ java-spring-ru/jpa-auditing/src/main/java/exercise/model/Task.java
@@ -0,0 +1,15 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+// BEGIN
+
+// END
