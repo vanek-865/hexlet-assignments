1462c843ddeefb6823c296e65a6f0b65615e007a
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 18:36:57 +0300

    Add validate guest

--- java-spring-ru/entity-validation/src/main/java/exercise/model/Guest.java
+++ java-spring-ru/entity-validation/src/main/java/exercise/model/Guest.java
@@ -8,10 +8,13 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -34,7 +37,22 @@ public class Guest {
     private long id;
 
     // BEGIN
-    
+    @NotNull(message = "name is null")
+    private String name;
+
+    @Email(message = "email not valid")
+    private String email;
+
+    @Size(min = 11, max = 13, message = "phone number not valid size")
+    @Pattern(regexp = "^\\+.*\\d$", message = "phone number not valid")
+    private String phoneNumber;
+
+    @Pattern(regexp = "^\\d{4}$", message = "card number not valid")
+    private String clubCard;
+
+    @NotNull(message = "card is null")
+    @Future(message = "card not valid")
+    private LocalDate cardValidUntil;
     // END
 
     @CreatedDate
