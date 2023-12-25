c64ddf3960823e4bf6e9824f57a4995e387a49d2
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 12:55:46 +0300

    Add welcome method

--- java-spring-ru/beans/src/main/java/exercise/controller/WelcomeController.java
+++ java-spring-ru/beans/src/main/java/exercise/controller/WelcomeController.java
@@ -1,8 +1,21 @@
 package exercise.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import exercise.daytime.Daytime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    Daytime daytime;
+
+    @GetMapping(path = "/welcome")
+    public String welcome() {
+        return "It is " + daytime.getName() + " now! Welcome to Spring!";
+    }
+}
 
 // END
