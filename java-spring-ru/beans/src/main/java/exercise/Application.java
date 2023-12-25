d4cfe9daeae1a8bd7c800f80cbf5a4db9d83c9a8
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 12:55:29 +0300

    Add daytime bean

--- java-spring-ru/beans/src/main/java/exercise/Application.java
+++ java-spring-ru/beans/src/main/java/exercise/Application.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import exercise.daytime.Daytime;
 import exercise.daytime.Day;
 import exercise.daytime.Night;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.annotation.RequestScope;
 
 // BEGIN
 
@@ -21,6 +24,17 @@ public class Application {
     }
 
     // BEGIN
-    
+
+    @Bean
+    @RequestScope
+    Daytime getDaytime() {
+        int hour = LocalDateTime.now().getHour();
+
+        if (hour >= 6 && hour < 22) {
+            return new Day();
+        }
+
+        return new Night();
+    }
     // END
 }
