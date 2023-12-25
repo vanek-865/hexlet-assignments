5f02b15eb07a6a9cea174bd1167ef6a29f6828e7
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 15:33:29 +0300

    Add post dto

--- java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
+++ java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
@@ -5,6 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
+@Getter
+@Setter
+public class PostDTO {
 
+    private long id;
+    private String title;
+    private String body;
+    List<CommentDTO> comments;
+}
 
 // END
