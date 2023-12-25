dfc70e9584ab5b7cd7a037f94a435f53ce51a25e
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 11:44:24 +0300

    Add find by price methods

--- java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
+++ java-spring-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -1,15 +1,20 @@
 package exercise.repository;
 
+import exercise.model.Product;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
-import exercise.model.Product;
 
-import org.springframework.data.domain.Sort;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // BEGIN
-    
+
+    List<Product> findByPriceBetween(int startPrice, int endPrice, Sort price);
+
+    List<Product> findByPriceGreaterThan(int price, Sort by);
+
+    List<Product> findByPriceIsLessThan(int price, Sort by);
     // END
 }
