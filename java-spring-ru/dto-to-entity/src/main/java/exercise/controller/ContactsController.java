27999d9c2eded26c18c15ee6db88a57f6c24beb9
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 16:25:33 +0300

    Add contact post api

--- java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
+++ java-spring-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -1,26 +1,54 @@
 package exercise.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import exercise.dto.ContactCreateDTO;
+import exercise.dto.ContactDTO;
 import exercise.model.Contact;
 import exercise.repository.ContactRepository;
-import exercise.dto.ContactDTO;
-import exercise.dto.ContactCreateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contacts")
+@ResponseStatus(HttpStatus.CREATED)
 public class ContactsController {
 
     @Autowired
     private ContactRepository contactRepository;
 
     // BEGIN
-    
+    @PostMapping(path = "")
+    public ContactDTO create(@RequestBody ContactCreateDTO contactData) {
+
+        Contact contact = contactRepository.save(toEntity(contactData));
+        return toContactDTO(contact);
+    }
+
+    private Contact toEntity(ContactCreateDTO contactCreateDTO) {
+
+        Contact contact = new Contact();
+        contact.setPhone(contactCreateDTO.getPhone());
+        contact.setFirstName(contactCreateDTO.getFirstName());
+        contact.setLastName(contactCreateDTO.getLastName());
+
+        return contact;
+    }
+
+    private ContactDTO toContactDTO(Contact contact) {
+
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setPhone(contact.getPhone());
+        contactDTO.setFirstName(contact.getFirstName());
+        contactDTO.setLastName(contact.getLastName());
+        contactDTO.setId(contact.getId());
+        contactDTO.setUpdatedAt(contact.getUpdatedAt());
+        contactDTO.setCreatedAt(contact.getCreatedAt());
+
+        return contactDTO;
+    }
     // END
 }
