898d7bd5f3a4347459858fafaaaad83f21d3f9bf
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 11:44:47 +0300

    Add find by price rest api

--- java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -1,18 +1,17 @@
 package exercise.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.data.domain.Sort;
-
-import java.util.List;
-
+import exercise.exception.ResourceNotFoundException;
 import exercise.model.Product;
 import exercise.repository.ProductRepository;
-import exercise.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -22,14 +21,31 @@ public class ProductsController {
     private ProductRepository productRepository;
 
     // BEGIN
-    
+    @GetMapping(path = "")
+    public List<Product> findByPrice(
+            @RequestParam(required = false) Integer min,
+            @RequestParam(required = false) Integer max) {
+
+        List<Product> product;
+        if (min == null && max == null) {
+            product = productRepository.findAll(Sort.by(Sort.Order.asc("price")));
+        } else if (min == null) {
+            product = productRepository.findByPriceIsLessThan(max, Sort.by(Sort.Order.asc("price")));
+        } else if (max == null) {
+            product = productRepository.findByPriceGreaterThan(min, Sort.by(Sort.Order.asc("price")));
+        } else {
+            product = productRepository.findByPriceBetween(min, max, Sort.by(Sort.Order.asc("price")));
+        }
+
+        return product;
+    }
     // END
 
     @GetMapping(path = "/{id}")
     public Product show(@PathVariable long id) {
 
-        var product =  productRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+        var product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
 
         return product;
     }
