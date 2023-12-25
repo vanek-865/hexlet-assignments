b38322d9e795a60d10b990fcc94bc0f19f347199
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 16:57:30 +0300

    Add dependency mapstruct

--- java-spring-ru/dto-mapper/build.gradle.kts
+++ java-spring-ru/dto-mapper/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
 }
 
