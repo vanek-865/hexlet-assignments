108099d98abe7225c12b2283ae3f056dbba429fc
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 17:28:33 +0300

    Add mapper

--- java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -1,5 +1,39 @@
 package exercise.mapper;
 
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.model.Product;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
 // BEGIN
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class ProductMapper {
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "barcode", source = "vendorCode")
+    public abstract Product mapToEntity(ProductCreateDTO dto);
+
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "price", source = "cost")
+    @Mapping(target = "vendorCode", source = "barcode")
+    public abstract ProductDTO mapToDto(Product product);
+
+    @Mapping(target = "cost", source = "price")
+    public abstract void update(ProductUpdateDTO dto, @MappingTarget Product product);
+
+}
 
 // END
