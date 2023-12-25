83307ddd6f10afeaeb4b85e72db4f2ab7e2f8000
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-25 16:55:44 +0300

    Init

---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,32 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.List;
+
+import exercise.repository.ProductRepository;
+import exercise.dto.ProductDTO;
+import exercise.dto.ProductCreateDTO;
+import exercise.dto.ProductUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
+import exercise.mapper.ProductMapper;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // BEGIN
+    
+    // END
+}
---
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -0,0 +1,5 @@
+package exercise.mapper;
+
+// BEGIN
+
+// END
