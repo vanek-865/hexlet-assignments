21030fb6ec92095f5a38a6ad3527769cbc90cbbe
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 09:05:37 +0300

    Add car mapper

--- java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -12,5 +12,19 @@ import exercise.dto.CarDTO;
 import exercise.model.Car;
 
 // BEGIN
+@Mapper(
+        uses = { JsonNullableMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class CarMapper {
+
+    public abstract CarDTO map(Car car);
+
+    public abstract Car map(CarCreateDTO car);
+
+    public abstract void update(CarUpdateDTO carUpdateDTO, @MappingTarget Car car);
+}
 
 // END
