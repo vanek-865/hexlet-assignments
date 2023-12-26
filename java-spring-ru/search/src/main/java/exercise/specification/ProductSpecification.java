2b68abf054b52c7b303a17dcfee2c5e5ea738823
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 17:02:18 +0300

    Add product specification

--- java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -7,5 +7,42 @@ import exercise.dto.ProductParamsDTO;
 import exercise.model.Product;
 
 // BEGIN
+@Component
+public class ProductSpecification {
 
+    public Specification<Product> build(ProductParamsDTO params) {
+
+        return withTitleCont(params.getTitleCont())
+                .and(withCategoryId(params.getCategoryId()))
+                .and(withPriceLt(params.getPriceLt()))
+                .and(withPriceeGt(params.getPriceGt()))
+                .and(withRatingGt(params.getRatingGt()));
+    }
+
+    private Specification<Product> withTitleCont(String titleCont){
+
+        return (root, query, cb) -> titleCont == null ? cb.conjunction() : cb.like(root.get("title"), titleCont);
+    }
+
+    private Specification<Product> withCategoryId(Long categoryId){
+
+        return (root, query, cb) -> categoryId == null ? cb.conjunction() : cb.equal(root.get("category").get("id"), categoryId);
+    }
+
+    private Specification<Product> withPriceLt(Integer price){
+
+        return (root, query, cb) -> price == null ? cb.conjunction() : cb.lessThan(root.get("price"), price);
+    }
+
+    private Specification<Product> withPriceeGt(Integer price){
+
+        return (root, query, cb) -> price == null ? cb.conjunction() : cb.greaterThan(root.get("price"), price);
+    }
+
+    private Specification<Product> withRatingGt(Double rating){
+
+        return (root, query, cb) -> rating == null ? cb.conjunction() : cb.greaterThan(root.get("rating"), rating);
+    }
+
+}
 // END
