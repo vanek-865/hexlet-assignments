26327c5f50ef3355d238b308f9ac6861afd206eb
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 09:25:26 +0300

    Init

---
+++ java-spring-ru/one-to-many/src/main/java/exercise/mapper/TaskMapper.java
@@ -0,0 +1,25 @@
+package exercise.mapper;
+
+import exercise.dto.TaskCreateDTO;
+import exercise.dto.TaskDTO;
+import exercise.dto.TaskUpdateDTO;
+import exercise.model.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    // BEGIN
+    
+    // END
+
+}
