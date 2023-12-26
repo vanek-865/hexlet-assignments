cadbfc43988ee15a266af941bff2d26ab0f579f3
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 09:05:25 +0300

    Add car update dto

--- java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
+++ java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -1,11 +1,17 @@
 package exercise.dto;
 
-import org.openapitools.jackson.nullable.JsonNullable;
-
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 // BEGIN
+@Getter
+@Setter
+public class CarUpdateDTO {
+
+    private JsonNullable<String> model;
+    private JsonNullable<String> manufacturer;
+    private JsonNullable<Integer> enginePower;
+}
 
 // END
