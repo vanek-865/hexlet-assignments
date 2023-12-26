55d170ac6aff88a95a2b74351d339148f4915c52
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 08:46:00 +0300

    Init

---
+++ java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -0,0 +1,11 @@
+package exercise.dto;
+
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+
+// END
---
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -0,0 +1,16 @@
+package exercise.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import exercise.dto.CarCreateDTO;
+import exercise.dto.CarUpdateDTO;
+import exercise.dto.CarDTO;
+import exercise.model.Car;
+
+// BEGIN
+
+// END
