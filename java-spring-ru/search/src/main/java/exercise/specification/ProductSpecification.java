bc88b94e204959873d035a7da527c3d056b20f9f
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-26 16:25:47 +0300

    Init

---
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -0,0 +1,11 @@
+package exercise.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import exercise.dto.ProductParamsDTO;
+import exercise.model.Product;
+
+// BEGIN
+
+// END
