4a720a9ae8db0e77a06ca80059dbaf27e90bf813
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-27 13:04:48 +0300

    Add author service

--- java-spring-ru/service-layer/settings.gradle.kts
+++ java-spring-ru/service-layer/settings.gradle.kts
@@ -1,3 +1,3 @@
-rootProject.name = "one-to-many"
+rootProject.name = "service-layer"
 // spring.jpa.generate-ddl = true
 // spring.jpa.hibernate.ddl-auto
--- java-spring-ru/service-layer/src/main/java/exercise/service/AuthorService.java
+++ java-spring-ru/service-layer/src/main/java/exercise/service/AuthorService.java
@@ -7,6 +7,7 @@ import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.AuthorMapper;
 import exercise.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,47 @@ import java.util.List;
 @Service
 public class AuthorService {
     // BEGIN
-    
+    @Autowired
+    private AuthorMapper authorMapper;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<AuthorDTO> getAll(int page, int size){
+
+        var authors = authorRepository.findAll(PageRequest.of(page - 1, size));
+        return authors.stream().map(authorMapper::map).toList();
+    }
+
+    public AuthorDTO create (AuthorCreateDTO authorCreateDTO){
+
+        var author = authorMapper.map(authorCreateDTO);
+        authorRepository.save(author);
+
+        return authorMapper.map(author);
+    }
+
+    public AuthorDTO findById(Long id) {
+
+        var author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+
+        return authorMapper.map(author);
+    }
+
+    public AuthorDTO update(Long id, AuthorUpdateDTO authorUpdateDTO) {
+
+        var author = authorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+        authorMapper.update(authorUpdateDTO,author);
+        authorRepository.save(author);
+
+        return authorMapper.map(author);
+    }
+
+    public void delete(Long id) {
+
+        authorRepository.deleteById(id);
+    }
     // END
 }
