51355b5f9a96904d6700f34f256ac1c18e4a704a
Noskov Ivan <Ivan.S.NOSKOV@raiffeisen.ru>
2023-12-27 13:25:14 +0300

    Add book service

--- java-spring-ru/service-layer/src/main/java/exercise/service/BookService.java
+++ java-spring-ru/service-layer/src/main/java/exercise/service/BookService.java
@@ -7,6 +7,7 @@ import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.BookMapper;
 import exercise.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,44 @@ import java.util.List;
 @Service
 public class BookService {
     // BEGIN
-    
+    @Autowired
+    private BookMapper bookMapper;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<BookDTO> getAll(int page, int size) {
+        var books = bookRepository.findAll(PageRequest.of(page - 1, size));
+        return books.stream().map(bookMapper::map).toList();
+    }
+
+    public BookDTO findById(Long id) {
+        var book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found " + id));
+        return bookMapper.map(book);
+    }
+
+    public BookDTO create(BookCreateDTO bookCreateDTO) {
+
+        var book = bookMapper.map(bookCreateDTO);
+        bookRepository.save(book);
+
+        return bookMapper.map(book);
+    }
+
+    public BookDTO update(Long id, BookUpdateDTO bookUpdateDTO) {
+
+        var book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
+        bookMapper.update(bookUpdateDTO, book);
+        bookRepository.save(book);
+
+        return bookMapper.map(book);
+    }
+
+    public void delete(Long id) {
+
+        bookRepository.deleteById(id);
+    }
     // END
 }
